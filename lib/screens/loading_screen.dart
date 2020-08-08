9111170f6c2f11bf5f9696111015c07cb5e39a67
Nagaraj U <nagarajullegaddi01@gmail.com>
2020-08-08 23:02:16 +0530

    final update

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,11 @@
+import 'package:clima/services/weather.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:clima/services/networking.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -12,52 +15,27 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
-  }
+  void getLocationData() async {
+    WeatherModel weatherModel = WeatherModel();
+    var weatherData = await weatherModel.getLocationWeather();
 
-  void getData() async{
-    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02');
-    if(response.statusCode == 200){
-      String data = response.body;
-      //print(data);
-//      var lon = jsonDecode(data)['coord']['lon'];
-//      print(lon);
-//
-//      var weatherDescription = jsonDecode(data)['weather'][0]['description'];
-//      print(weatherDescription);
-
-      var temp = jsonDecode(data)['main']['temp'];
-      var id = jsonDecode(data)['id'];
-      var cityName = jsonDecode(data)['name'];
-      print(cityName);
-      print(id);
-      print(temp);
-
-    }else{
-      print(response.statusCode);
-    }
-
-
-
+    Navigator.push(context, MaterialPageRoute(builder : (context){
+      return LocationScreen(locationWeather: weatherData);
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    String myStr = '15';
-    getData();
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.all(double.parse(myStr)),
-        color: Colors.red,
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white70,
+          size: 100,
+        ),
       ),
     );
   }
